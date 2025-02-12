07c8327062141c64a8f3d55047e3c381352f8470
twahir21 <twahirsudy3@gmail.com>
2025-02-12 16:43:48 +0300

    fixing assignRole links

--- backend/controllers/assignRoleController.js
+++ backend/controllers/assignRoleController.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePath = path.join(__dirname, 'json', 'tempUsers.json');
+const filePath = path.join(__dirname, '../routes/json', 'tempUsers.json');
 
 
 // Utility function to read JSON data
