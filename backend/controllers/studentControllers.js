ae5f04a5a045dad73517f6b0b5ba64a50764d44a
twahir21 <twahirsudy3@gmail.com>
2025-02-24 21:32:22 +0300

    implementing fetching student based on id

--- backend/controllers/studentControllers.js
+++ backend/controllers/studentControllers.js
@@ -18,3 +18,24 @@ export const fetchStudents = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+// fetch a single student
+export const fetchSingleStudent = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await database.query(`
+            SELECT * FROM students 
+            WHERE id = $1       
+            `, [id]);
+        if (response.rows.length === 0) {
+            res.status(404).json({ message: "Student not found in Database" });
+            return;
+        }
+        const data = response.rows;
+
+        res.status(200).json({ data: data });
+    } catch (error) {
+        res.status(500).json({ message: error.message })  
+    }
+}
--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { fetchStudents } from "../controllers/studentControllers.js";
+import { fetchSingleStudent, fetchStudents } from "../controllers/studentControllers.js";
 
 const studentRouter = express.Router();
 
 
 // fetch all students
 studentRouter.get("/students", fetchStudents);
+
+// fetch one student
+studentRouter.get("/students/:id", fetchSingleStudent)
 
 export default studentRouter;
